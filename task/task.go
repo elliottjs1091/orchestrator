28714155d7695b158bf461782c448305d2db87a2
James Elliott <elliottjs1091@gmail.com>
2025-12-01 21:28:35 +1300

    Added types: - Docker - DockerResult
    Added Run() function that takes DockerResult as a parameter
    
    Added missing modules, specifically moby/client

--- task/task.go
+++ task/task.go
@@ -1,10 +1,15 @@
 package task
 
 import (
+	"context"
+	"io"
+	"log"
+	"os"
 	"time"
 
 	"github.com/docker/go-connections/nat"
 	"github.com/google/uuid"
+	"github.com/moby/moby/client"
 )
 
 // universally unique identifiers
@@ -21,7 +26,7 @@ const (
 
 type Task struct {
 	ID            uuid.UUID
-	ContainerID   string
+	ContainerId   string
 	Name          string
 	State         State
 	Image         string
@@ -55,4 +60,27 @@ type Config struct { // Holds configuration for tasks
 	Disk           int64
 	Env            []string
 	RestartPolicy  string
+}
+
+type Docker struct { // Uses Task struct from above - ContainerId field will be used to interact with containers
+	Client *client.Client // Used to interact with Docker API
+	Config Config
+}
+
+type DockerResult struct {
+	Error       error
+	Action      string
+	ContainerId string
+	Result      string
+}
+
+func (d *Docker) Run() DockerResult {
+	ctx := context.Background()
+	reader, err := d.Client.ImagePull(
+		ctx, d.Config.Image, types.ImagePullOptions{})
+	if err != nil {
+		log.Printf("Error pulling image %s: %v\n", d.Config.Image, err)
+		return DockerResult{Error: err}
+	}
+	io.Copy(os.Stdout, reader)
 }
