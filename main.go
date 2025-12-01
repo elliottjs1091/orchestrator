44e7148cae4c09b35893007393c894ba831851fd
James Elliott <elliottjs1091@gmail.com>
2025-12-01 23:43:55 +1300

    Called createContainer and stopContainer functions in main() function

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"orchestrator/node"
 	"orchestrator/task"
 	"orchestrator/worker"
+	"os"
 	"time"
 
 	"github.com/golang-collections/collections/queue"
@@ -67,6 +68,16 @@ func main() {
 	}
 
 	fmt.Printf("node: %v\n", n)
+
+	fmt.Printf("create a test container\n")
+	dockerTask, createResult := createContainer()
+	if createResult.Error != nil {
+		fmt.Printf("%v, createResult.Error")
+		os.Exit(1)
+	}
+	time.Sleep(time.Second * 5)
+	fmt.Printf("stopping container%s\n", createResult.ContainerId)
+	_ = stopContainer(dockerTask, createResult.ContainerId)
 }
 
 func createContainer() (*task.Docker, *task.DockerResult) {
@@ -78,7 +89,7 @@ func createContainer() (*task.Docker, *task.DockerResult) {
 			"POSTGRES_PASSWORD=secret", // Uh okay...
 		},
 	}
-	dc, _ := client.NewClientWithOpts(client.FromEnv)
+	dc, _ := client.New(client.FromEnv)
 	d := task.Docker{
 		Client: dc,
 		Config: c,
@@ -93,13 +104,13 @@ func createContainer() (*task.Docker, *task.DockerResult) {
 	return &d, &result
 }
 
-func stopContainer (d *task.Docker, id string) *task.DockerResult {
+func stopContainer(d *task.Docker, id string) *task.DockerResult {
 	result := d.Docker.Stop(id)
 	if result.Error != nil {
 		fmt.Printf("%v\n", result.Error)
 		return nil
 	}
 	fmt.Printf(
-		"Container %s has been stopped and removed\n", result.ContainerId	)
-		return &result}
+		"Container %s has been stopped and removed\n", result.ContainerId)
+	return &result
 }
