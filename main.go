5df713af508f17608bddfbccfe50e2b10acfa374
James Elliott <elliottjs1091@gmail.com>
2025-12-01 23:28:40 +1300

    Added CreateContainer method

--- main.go
+++ main.go
@@ -10,6 +10,7 @@ import (
 
 	"github.com/golang-collections/collections/queue"
 	"github.com/google/uuid"
+	"github.com/moby/moby/client"
 )
 
 func main() {
@@ -66,4 +67,28 @@ func main() {
 	}
 
 	fmt.Printf("node: %v\n", n)
+}
+
+func createContainer() (*task.Docker, *task.DockerResult) {
+	c := task.Config{
+		Name:  "test-container-1",
+		Image: "postgres:13",
+		Env: []string{
+			"POSTGRES_USER=orchestrator",
+			"POSTGRES_PASSWORD=secret", // Uh okay...
+		},
+	}
+	dc, _ := client.NewClientWithOpts(client.FromEnv)
+	d := task.Docker{
+		Client: dc,
+		Config: c,
+	}
+	result := d.Run()
+	if result.Error != nil {
+		fmt.Printf("%v\n", result.Error)
+		return nil, nil
+	}
+	fmt.Printf(
+		"Container %s is running with config %v\n", result.ContainerId, c)
+	return &d, &result
 }
